c8e6959645561175db0a7a3d5aed06a136a0b6f9
Dooy94 <dooy94@gmail.com>
2017-03-21 16:22:22 +0900

    Update Average.java
    20130672 윤두용 입니다.

--- Average.java
+++ Average.java
@@ -1,16 +1,27 @@
+package q1;
 import java.util.Scanner;
 
-public class Average { 
+public class Average { 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.print("입력할 최대 실수의 갯수를 입력하세요. >> ");    
-        Scanner input = new Scanner(System.in);  
-        int size = input.nextInt();    
-        System.out.println("\n중간에 0을 입력하면 이전 입력자료로 계산하고 종료합니다.");  
-        System.out.print("실수 " + size + "개를 입력하세요. >> ");    
+        System.out.print("입력할 최대 실수의 갯수를 입력하세요. >> ");    
+        Scanner input = new Scanner(System.in);  
+        int size = input.nextInt();    
+        System.out.println("\n중간에 0을 입력하면 이전 입력자료로 계산하고 종료합니다.");  
+        System.out.print("실수 " + size + "개를 입력하세요. >> ");    
+    
+        double ary[] = new double[size];
+        double sum=0.0;
         
-        //프로그래밍 하세요.
-        
-        System.out.println("합: " + sum + ", 평균: " + sum/size);
+        if(input.nextDouble()==0) break;
+        	
+    
+        else if(input.nextDouble()>0) continue;
+        		
+        		for(int i=0;i<size;i++){
+            	ary[i]=input.nextDouble();
+            	sum+=ary[i];
+        		}
+         System.out.println("합: " + sum + ", 평균: " + sum/size);
     }
 }
